b185ab2a650fd7debe744649d382176ec0d69131
inselc <13700483+inselc@users.noreply.github.com>
2019-11-01 18:05:08 +0100

    Ansteuerung der Status-LEDs mit Blinkmuster hinzugefügt

---
+++ src/userlib/BlinkSequencer/BlinkSequencer.c
@@ -0,0 +1,66 @@
+#include "stm8l15x.h"
+#include "BlinkSequencer.h"
+
+/*- Typdefinitionen ----------------------------------------------------------*/
+typedef struct tag_Blink_LedGpio
+{
+  GPIO_TypeDef* pGpio;
+  uint8_t ucPin;
+} Blink_LedGpio;
+
+
+/*- Modulglobale Variablen ---------------------------------------------------*/
+volatile Blink_LedGpio asBlinkGPIO[Blink_MaxLed] = 
+{
+  {GPIOB, GPIO_Pin_5}, /* SYS LED */
+  {GPIOB, GPIO_Pin_7}  /* BT LED */
+};
+
+volatile uint16_t uiBlinkPattern[Blink_MaxLed];
+volatile uint16_t uiNextBlinkPattern[Blink_MaxLed];
+volatile uint8_t ucPatternIndex;
+
+
+void Blink_Init(void)
+{
+  uint8_t ucIndex;
+  
+  for (ucIndex = 0; ucIndex < Blink_MaxLed; ++ucIndex)
+  {
+    GPIO_Init(asBlinkGPIO[ucIndex].pGpio, asBlinkGPIO[ucIndex].ucPin, GPIO_Mode_Out_PP_Low_Slow);
+    uiBlinkPattern[ucIndex] = 0;
+    uiNextBlinkPattern[ucIndex] = 0;
+  }
+
+  ucPatternIndex = 0;
+}
+
+void Blink_SetPattern(Blink_Led eLed, uint16_t uiPattern)
+{
+  uiNextBlinkPattern[eLed] = uiPattern;
+}
+
+bool Blink_Ready(Blink_Led eLed)
+{
+  return (uiBlinkPattern[eLed] == uiNextBlinkPattern[eLed]);
+}
+
+void Blink_Poll(void)
+{
+  uint8_t ucIndex;
+  for (ucIndex = 0; ucIndex < Blink_MaxLed; ++ucIndex)
+  {
+    BitAction bBitState = (uiBlinkPattern[ucIndex] & (1 << ucPatternIndex)) == (1 << ucPatternIndex);
+    GPIO_WriteBit(asBlinkGPIO[ucIndex].pGpio, asBlinkGPIO[ucIndex].ucPin, bBitState);
+  }
+  
+  ++ucPatternIndex;
+  if (ucPatternIndex > 15)
+  {
+    ucPatternIndex = 0;
+    for (ucIndex = 0; ucIndex < Blink_MaxLed; ++ucIndex)
+    {
+      uiBlinkPattern[ucIndex] = uiNextBlinkPattern[ucIndex];
+    }
+  }
+}
---
+++ src/userlib/BlinkSequencer/BlinkSequencer.h
@@ -0,0 +1,24 @@
+#ifndef USERLIB_BLINKSEQUENCER_H_
+#define USERLIB_BLINKSEQUENCER_H_
+
+/*- Headerdateien ------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+
+
+/*- Typdefinitionen ----------------------------------------------------------*/
+typedef enum tag_Blink_Led
+{
+  Blink_Led_SYS = 0,
+  Blink_Led_BT,
+  Blink_MaxLed
+} Blink_Led;
+
+
+/*- Funktionsprototypen ------------------------------------------------------*/
+void Blink_Init(void);
+void Blink_SetPattern(Blink_Led eLed, uint16_t uiPattern);
+bool Blink_Ready(Blink_Led eLed);
+void Blink_Poll(void);
+
+#endif /* USERLIB_BLINKSEQUENCER_H_ */
