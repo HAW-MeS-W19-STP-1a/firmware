33b25cf5e2a0e4ec5fe48cb9ae05eb1c562c47e6
inselc <13700483+inselc@users.noreply.github.com>
2019-11-01 18:04:45 +0100

    Sammelheader für CommLib hinzugefügt

---
+++ src/commlib/commlib.h
@@ -0,0 +1,19 @@
+/*!****************************************************************************
+ * @file
+ * commlib.h
+ *
+ * Sammelheader zum Einbinden der verfügbaren Schnittstellen
+ *
+ * @date  31.10.2019
+ ******************************************************************************/
+
+#ifndef COMMLIB_H_
+#define COMMLIB_H_
+
+/*- Headerdateien ------------------------------------------------------------*/
+#include "commlib_i2c.h"
+#include "commlib_uart1.h"
+#include "commlib_uart2.h"
+#include "commlib_uart3.h"
+
+#endif /* COMMLIB_H_ */
