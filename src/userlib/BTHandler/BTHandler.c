5f7f28915239514499a9221d70d464c044aff133
inselc <13700483+inselc@users.noreply.github.com>
2019-12-30 13:38:18 +0100

    BT Pins Zuordnung über IO_Map

--- src/userlib/BTHandler/BTHandler.c
+++ src/userlib/BTHandler/BTHandler.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "stm8l15x.h"
 #include "commlib.h"
+#include "io_map.h"
 #include "BlinkSequencer.h"
+
 
 /*- Symbolische Konstanten ---------------------------------------------------*/
 #define USERLIB_BTHANDLER_ADTIME  30
@@ -26,11 +28,11 @@ uint8_t ucAdTimer;
 
 void BTHandler_Init(void)
 {
-  GPIO_Init(GPIOC, GPIO_Pin_2, GPIO_Mode_In_FL_No_IT);      // Modul TXD an PC2
-  GPIO_Init(GPIOC, GPIO_Pin_3, GPIO_Mode_Out_PP_High_Fast);  // Modul RXD an PC3
-  GPIO_Init(GPIOC, GPIO_Pin_4, GPIO_Mode_Out_PP_Low_Slow); // Modul "EN" (CMD) an PC4
-  GPIO_Init(GPIOD, GPIO_Pin_1, GPIO_Mode_In_FL_No_IT); // Modul "Status" an PD1
-  GPIO_Init(GPIOD, GPIO_Pin_3, GPIO_Mode_Out_PP_High_Slow); // Modul "PWREN" an PD3
+  GPIO_Init(USART2_BT_PORT, USART3_BT_TX_PIN, GPIO_Mode_In_FL_No_IT);      // Modul TXD an PC2
+  GPIO_Init(USART2_BT_PORT, USART2_BT_RX_PIN, GPIO_Mode_Out_PP_High_Fast);  // Modul RXD an PC3
+  GPIO_Init(BT_CMD_PORT, BT_CMD_PIN, GPIO_Mode_Out_PP_Low_Slow); // Modul "EN" (CMD) an PC4
+  GPIO_Init(BT_STAT_PORT, BT_STAT_PIN, GPIO_Mode_In_FL_No_IT); // Modul "Status" an PD1
+  GPIO_Init(BT_PWREN_PORT, BT_PWREN_PIN, GPIO_Mode_Out_PP_High_Slow); // Modul "PWREN" an PD3
   UART1_Init();
   
   printf("BTHandler ON\r\n");
@@ -80,7 +82,7 @@ void BTHandler_Task1s(void)
     {
       /* Advertising Timer abgelaufen ohne Verbindung     */
       printf("BTHandler OFF\r\n");
-      GPIO_WriteBit(GPIOD, GPIO_Pin_3, false);
+      GPIO_WriteBit(BT_PWREN_PORT, BT_PWREN_PIN, DISABLE);
       eState = BTHandler_State_OFF;
       Blink_SetPattern(Blink_Led_BT, USERLIB_BTHANDLER_LED_OFF);
     }
@@ -93,7 +95,7 @@ void BTHandler_TakeWakeup(void)
   {
     /* Advertising für einige Sekunden einschalten        */
     printf("BTHandler ON\r\n");
-    GPIO_WriteBit(GPIOD, GPIO_Pin_3, true);
+    GPIO_WriteBit(BT_PWREN_PORT, BT_PWREN_PIN, ENABLE);
     ucAdTimer = USERLIB_BTHANDLER_ADTIME;
     eState = BTHandler_State_ADVERTISE;   
     Blink_SetPattern(Blink_Led_BT, USERLIB_BTHANDLER_LED_ADV);
